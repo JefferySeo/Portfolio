dc438b2fc83f5d02184a225216ca5001ce0a3c46
JefferySeo <125331815+JefferySeo@users.noreply.github.com>
2023-07-13 15:38:03 +0900

    about 수정

--- src/main/frontend/src/component/Navigation.jsx
+++ src/main/frontend/src/component/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
 import work from '../data/work';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     
@@ -11,15 +12,15 @@ const Navigation = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto text-end">
-                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link><Link to="/about">About</Link></Nav.Link>
                         <NavDropdown title='Portfolio' id="show_portfolio">
                             {work.portfolio.map((dt, index) => {
                                 let url = "/portfolio/" + dt.id;
-                                return <NavDropdown.Item className='text-end' href={url} key={index}>{dt.main_title}</NavDropdown.Item>
+                                return <NavDropdown.Item className='text-end' key={index}><Link to={url}>{dt.main_title}</Link></NavDropdown.Item>
                             })}
                             
                         </NavDropdown>
-                        <Nav.Link href="/contact">Contact</Nav.Link>
+                        <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
                         <Nav.Link target={'_blank'} href="https://github.com/JefferySeo">Github</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
